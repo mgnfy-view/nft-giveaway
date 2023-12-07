cc0e9fc8ec2cd8c5c999111fa211f51fb21fca75
Sahil-Gujrati <sahilgujrati12@gmail.com>
2023-12-07 13:14:25 +0530

    refactor: Minor changes

--- contracts/AwesomeNFT.sol
+++ contracts/AwesomeNFT.sol
@@ -4,21 +4,21 @@ pragma solidity ^0.8.19;
 
 error AwesomeNFT__RewardMintedGiveawayClosed();
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AwesomeNFT is ERC721URIStorage, Ownable {
     uint256 public tokenCounter = 0;
-    string public NFTURI;
+    string public nftUri;
 
-    constructor(string memory _NFTURI) ERC721("Awesome NFT", "ANFT") Ownable(msg.sender) {
-        NFTURI = _NFTURI;
+    constructor(string memory _nftUri) ERC721("Awesome NFT", "ANFT") Ownable(msg.sender) {
+        nftUri = _nftUri;
     }
 
     function mintReward(address player) public onlyOwner {
         if (tokenCounter != 0) revert AwesomeNFT__RewardMintedGiveawayClosed();
         _safeMint(player, tokenCounter);
-        _setTokenURI(tokenCounter, NFTURI);
+        _setTokenURI(tokenCounter, nftUri);
         tokenCounter++;
     }
 }
