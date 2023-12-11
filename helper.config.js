dd7ae09439ac6cd82d68885661f29f052a634655
Sahil-Gujrati <sahilgujrati12@gmail.com>
2023-12-11 15:02:06 +0530

    chore: Increase the giveaway's interval

--- helper.config.js
+++ helper.config.js
@@ -9,7 +9,7 @@ const networkConfig = {
         keyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 150 gwei keyHash
         vrfCoordinatorAddress: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         callbackGasLimit: "500000", // the maximum gas limit for fulfillRandomWords function
-        interval: "10", // the amount of time after which the giveaway winner will be selected (here, its set to 10 seconds)
+        interval: "90", // the amount of time after which the giveaway winner will be selected (here, its set to 10 seconds)
     },
     31337: {
         name: "localhost",
@@ -17,7 +17,7 @@ const networkConfig = {
         subscriptionId: 1,
         keyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
         callbackGasLimit: "5000000",
-        interval: "10",
+        interval: "90",
     },
 };
 
