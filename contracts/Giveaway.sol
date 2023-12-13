90b6776d64af21d6b6b4fc5146737a4596f05e9f
Sahil-Gujrati <sahilgujrati12@gmail.com>
2023-12-13 13:01:06 +0530

    feat: Now the contract can create a subscription, add itself as consumer, and register itself for upkeep. Also add methods to cleanup after the giveaway ends

--- contracts/Giveaway.sol
+++ contracts/Giveaway.sol
@@ -6,19 +6,23 @@ import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interface
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
 import {PrizeNFT} from "./PrizeNFT.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {RegistrationParams, AutomationRegistrarInterface} from "./AutomationRegistrarInterface.sol";
 
 error Giveaway__NotAValidAddress();
 error Giveaway__AlreadyJoined();
 error Giveaway__NotOpen();
 error Giveaway__UpkeepNotNeeded();
 error Giveaway__IndexOutOfBounds();
+error Giveaway__UpkeepRegistrationFailed();
 
 /**
  * @title A Giveaway smart contract
  * @author Sahil Gujrati
- * @notice People can join to have a fair chance at winning an NFT. The NFT can also represent an physical item such as sneakers, gaming consoles, etc
+ * @notice People can join to have a fair chance at winning an NFT. The NFT can also represent a physical asset such as sneakers, gaming consoles, etc
  */
-contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
+contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface, Ownable {
     /**
      * @notice Emitted once when the giveaway is deployed
      */
@@ -47,31 +51,40 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
         uint256 randomWord;
     }
 
+    /**
+     * @dev 0 for open state, 1 for the "selecting winner" state, and 2 for the closed state
+     */
     enum GiveawayState {
         OPEN,
         SELECTING_WINNER,
         CLOSED
     }
 
+    GiveawayState private giveawayState = GiveawayState.OPEN;
+    address private immutable giveawayOwner;
     uint256 private participantCount = 0;
     mapping(address => bool) private participants;
     address[] private participantsArray;
     address private winner;
-    GiveawayState private giveawayState = GiveawayState.OPEN;
     uint256 private immutable interval;
+    string private nftMetadataUri;
     uint256 private lastTimeStamp;
     VRFRequest private vrfRequest;
-    VRFCoordinatorV2Interface private immutable vrfCoordinator;
-    uint64 private immutable subscriptionId;
+
+    uint64 private subscriptionId;
     bytes32 private immutable keyHash;
     uint32 private immutable callbackGasLimit;
     uint16 private constant REQUEST_CONFIRMATIONS = 3; // the number of confirmations from chainlink nodes required before the random word is supplied. The higher the confirmations, the more secure is the random word
     uint32 private constant NUM_WORDS = 1; // the number of random words we want
+    address private immutable upkeepContractAddress;
+    uint256 private upkeepId;
+
+    VRFCoordinatorV2Interface private immutable vrfCoordinator;
+    AutomationRegistrarInterface private immutable registrar;
     PrizeNFT private immutable prizeNFT;
-    string private nftMetadataUri;
+    LinkTokenInterface private immutable linkToken;
 
     /**
-     * @param _subscriptionId The subscription ID that this contract uses to fund the vrf requests
      * @param _keyHash The gas lane key hash value, which is the maximum gas price you are willing to pay for a request in wei
      * @param _callbackGasLimit the amount of gas we want to use when the random word is supplied to our contract and fulfillRandomWords function is called
      * @param _interval The amount of time in seconds after which to randomly pick a winner
@@ -79,21 +92,30 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
      * @param _nftMetadataUri The ipfs uri that leads to the NFT metadata
      */
     constructor(
-        uint64 _subscriptionId,
         bytes32 _keyHash,
         uint32 _callbackGasLimit,
         uint256 _interval,
         address _vrfCoordinatorAddress,
-        string memory _nftMetadataUri
-    ) VRFConsumerBaseV2(_vrfCoordinatorAddress) {
-        subscriptionId = _subscriptionId;
+        string memory _nftMetadataUri,
+        address _linkTokenAddress,
+        address _automationRegistrarAddress
+    ) VRFConsumerBaseV2(_vrfCoordinatorAddress) Ownable(msg.sender) {
+        giveawayOwner = msg.sender;
+        nftMetadataUri = _nftMetadataUri;
+
         keyHash = _keyHash;
         callbackGasLimit = _callbackGasLimit;
         interval = _interval;
-        vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorAddress);
         lastTimeStamp = block.timestamp;
-        nftMetadataUri = _nftMetadataUri;
+
+        upkeepContractAddress = _automationRegistrarAddress;
+        vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorAddress);
+        registrar = AutomationRegistrarInterface(_automationRegistrarAddress);
         prizeNFT = new PrizeNFT(_nftMetadataUri);
+        linkToken = LinkTokenInterface(_linkTokenAddress);
+
+        createSubscription();
+        addGiveawayAsConsumer();
 
         emit GiveawayOpen();
     }
@@ -152,6 +174,79 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
     }
 
     /**
+     * @notice Creates a new subscription for the giveaway to use
+     */
+    function createSubscription() private {
+        subscriptionId = vrfCoordinator.createSubscription();
+    }
+
+    /**
+     * @notice Funds the subscription created by this contract on deployment for requesting a random number
+     * @param amount The amount of LINK tokens to fund the subscription with.
+     */
+    function fundSubscription(uint256 amount) external onlyOwner {
+        linkToken.transferAndCall(address(vrfCoordinator), amount, abi.encode(subscriptionId));
+    }
+
+    /**
+     * @notice Adds the giveaway contract as the consumer of the subscription created during deployment
+     */
+    function addGiveawayAsConsumer() private {
+        vrfCoordinator.addConsumer(subscriptionId, address(this));
+    }
+
+    /**
+     * @notice Removes the giveaway contract from the consumers list of the subscription created during deployment
+     */
+    function removeGiveawayFromConsumers() external onlyOwner {
+        vrfCoordinator.removeConsumer(subscriptionId, address(this));
+    }
+
+    /**
+     * @notice Cancels the subscription
+     * @dev sets the subscriptionId to 0
+     */
+    function cancelSubscription() external onlyOwner {
+        vrfCoordinator.cancelSubscription(subscriptionId, giveawayOwner);
+        subscriptionId = 0;
+    }
+
+    /**
+     * @notice Allows the owner to withdraw all the LINK tokens held by this contract
+     * @param to The address of the receiver of the LINK tokens
+     */
+    function withdraw(address to) external onlyOwner {
+        uint256 balance = linkToken.balanceOf(address(this));
+        linkToken.transfer(to, balance);
+    }
+
+    /**
+     * @notice Registers this contract for upkeep
+     * @param amount The amount to fund the upkeep with
+     */
+    function registerForUpkeep(uint96 amount) external onlyOwner {
+        RegistrationParams memory params;
+        params.name = "Giveaway";
+        params.encryptedEmail = "0x";
+        params.upkeepContract = address(this); // the contract that requires upkeep
+        params.gasLimit = 500000;
+        params.adminAddress = giveawayOwner;
+        params.triggerType = 0; // 0 for custom logic upkeep
+        params.checkData = "0x";
+        params.triggerConfig = "0x";
+        params.offchainConfig = "0x";
+        params.amount = amount;
+
+        linkToken.approve(address(registrar), params.amount);
+        uint256 _upkeepId = registrar.registerUpkeep(params);
+        if (_upkeepId != 0) {
+            upkeepId = _upkeepId;
+        } else {
+            revert Giveaway__UpkeepRegistrationFailed();
+        }
+    }
+
+    /**
      * @notice Allows anyone to join the giveaway for free.
      */
     function enterGiveaway() public {
@@ -161,6 +256,14 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
         participants[msg.sender] = true;
         participantsArray.push(msg.sender);
         participantCount++;
+    }
+
+    /**
+     * @notice Gets the address of the deployer of the giveaway
+     * @return The giveaway owner's address
+     */
+    function getGiveawayOwner() public view returns (address) {
+        return giveawayOwner;
     }
 
     /**
@@ -227,6 +330,14 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
     }
 
     /**
+     * @notice Tells the amount of time remaining before the giveaway's winner is selected
+     * @return Returns the time remaining before a winner is picked
+     */
+    function getRemainingTime() public view returns (uint256) {
+        return interval - (block.timestamp - lastTimeStamp);
+    }
+
+    /**
      * @notice Gets the interval after which the winner is to be picked
      * @return interval The interval (in seconds) after which the giveaway's winner will be selected
      */
@@ -235,11 +346,11 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
     }
 
     /**
-     * Tells the amount of time remaining before the giveaway's winner is selected
-     * @return Returns the time remaining before a winner is picked
+     * @notice Gets the subscription ID this contract uses for funding VRF requests
+     * @return The uint64 subscription ID
      */
-    function getRemainingTime() public view returns (uint256) {
-        return interval - (block.timestamp - lastTimeStamp);
+    function getSubscriptionId() public view returns (uint256) {
+        return subscriptionId;
     }
 
     /**
@@ -248,5 +359,13 @@ contract Giveaway is VRFConsumerBaseV2, AutomationCompatibleInterface {
      */
     function getVRFRequestDetails() public view returns (VRFRequest memory) {
         return vrfRequest;
+    }
+
+    /**
+     * @notice Gets the upkeep ID
+     * @return The upkeep ID that was given to this contract on registering for upkeep
+     */
+    function getUpkeepId() public view returns (uint256) {
+        return upkeepId;
     }
 }
